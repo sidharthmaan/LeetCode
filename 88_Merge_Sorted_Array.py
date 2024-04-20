fecd23ad38b7a6d2940a706f2799ad2747949dce
Sidharth <ssidharth984@gmail.com>
2024-04-20 12:48:11 +0530

    new file:   169_Majority_Element.py 	modified:   88_Merge_Sorted_Array.py

--- 88_Merge_Sorted_Array.py
+++ 88_Merge_Sorted_Array.py
@@ -1,7 +1,7 @@
 '''https://leetcode.com/problems/merge-sorted-array/?envType=study-plan-v2&envId=top-interview-150'''
 
 class Solution:
-    def merge(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
+    def merge(self, nums1: list[int], m: int, nums2: list[int], n: int) -> None:
         """
         Do not return anything, modify nums1 in-place instead.
         """
