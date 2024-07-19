43a2f651848443923cbe6c096ef3c387f93a4c99
Sidharth <ssidharth984@gmail.com>
2024-07-19 17:25:15 +0530

    new file:   array/4-Find_Pivot_Index.py

---
+++ array/4-Find_Pivot_Index.py
@@ -0,0 +1,13 @@
+'''https://leetcode.com/problems/find-pivot-index/description/'''
+
+
+class Solution(object):
+    def pivotIndex(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        for i in range(len(nums)):
+            if sum(nums[:i]) == sum(nums[i+1:]):
+                return i
+        return -1
